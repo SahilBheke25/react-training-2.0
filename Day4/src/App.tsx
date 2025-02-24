7c40e9b1876d07bf9dfca7f37bcfbeaea2d211f3
sahilbheke <sahil.bheke@joshsoftware.com>
2025-02-24 16:37:58 +0530

    Add Assignment Day4

---
+++ Day4/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import TodoApp from './pages/TodoApp';
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1 className="text-center my-3">Todo App</h1>
+      <TodoApp />
+    </div>
+  );
+}
+
+export default App
